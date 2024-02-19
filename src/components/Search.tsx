4f2aab6e2806d0cabd120b4b9cdbcab9c47ae7d8
lz-junior <correajr.dev@gmail.com>
2024-02-19 16:37:14 -0300

    component Search

---
+++ src/components/Search.tsx
@@ -0,0 +1,17 @@
+import { BsSearch } from "react-icons/bs";
+
+
+const Search = ()=> {
+  return (
+    <div>
+      <h2>Search for a user:</h2>
+      <p>See your best repositories</p>
+      <div>
+        <input type="text" placeholder="Enter the user name"/>
+        <button><BsSearch/></button>
+      </div>
+    </div>
+  )
+}
+
+export default Search;
