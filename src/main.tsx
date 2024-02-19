5e298a84d743739e6458978991a2c494a86a7a1e
lz-junior <correajr.dev@gmail.com>
2024-02-19 13:14:17 -0300

    start router dom

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
