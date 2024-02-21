c46e3a77a0a8ab0e0e490a9a85cd28ad8104f229
lz-junior <correajr.dev@gmail.com>
2024-02-21 17:33:17 -0300

    User tsx

---
+++ src/components/User.tsx
@@ -0,0 +1,29 @@
+import { UserProps } from "../types/user";
+import { MdLocationPin } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const User = ({login, avatar_url, followers, following, location,}: UserProps)=> {
+  return (
+    <div>
+      <img src={avatar_url} alt={login}></img>
+      <h2>{login}</h2>
+      <p>
+        <MdLocationPin />
+        <span>{location}</span>
+      </p>
+      <div>
+        <div>
+          <p>Followers:</p>
+          <p>{followers}</p>
+        </div>
+        <div>
+          <p>Following:</p>
+          <p>{following}</p>
+        </div>
+      </div>
+      <Link to={`/repos/${login}`}>See the best projects</Link>
+    </div>
+  )
+}
+
+export default User;
