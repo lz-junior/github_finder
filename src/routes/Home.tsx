c5eba485199ee10304f81f0ff8ee1504f0094af3
lz-junior <correajr.dev@gmail.com>
2024-02-22 12:33:30 -0300

    component Error - OK

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -11,6 +11,8 @@ const Home = ()=> {
   const [error, setError] = useState(false);
 
   const loadUser = async (userName: string)=> {
+    setError(false);
+    setUser(null);
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
 
